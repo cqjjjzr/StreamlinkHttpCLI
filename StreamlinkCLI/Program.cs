b999388a62bbb3988477f40c1aad941dfd7e2c63
Charlie Jiang <cqjjjzr@126.com>
2019-03-01 23:40:50 +0800

    add some func.

--- StreamlinkCLI/Program.cs
+++ StreamlinkCLI/Program.cs
@@ -1,42 +1,92 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using IniParser;
 
-namespace StreamlinkCLI
+namespace StreamlinkHttpCLI
 {
     public class Program
     {
         private static readonly Socket Listener = new Socket(SocketType.Stream, ProtocolType.Tcp);
         public static void Main(string[] args)
         {
+            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            Console.Title = "Streamlink Http CLI v" + version + " by Charlie Jiang";
             if (args.Length != 1)
             {
                 Console.WriteLine("Usage: StreamlinkHttpCLI <config_file>");
                 return;
             }
 
-            string channelUrl;
-            string proxy;
+            string liveUrl;
+            string proxy = null;
             string streamlink;
             var port = 15551;
+
+            string channelUrl = null;
             try
             {
                 var ini = new FileIniDataParser().ReadFile(args[0]);
                 var sec = ini.Sections["streamlink"];
-                streamlink = sec.GetKeyData("streamlink").Value;
-                proxy = sec.GetKeyData("proxy").Value;
-                channelUrl = sec.GetKeyData("url").Value;
+                if (!sec.ContainsKey("streamlink"))
+                    WriteLineWithColor("配置文件解析错误：未定义streamlink位置", ConsoleColor.Red);
+                streamlink = sec["streamlink"];
+                if (sec.ContainsKey("proxy"))
+                    proxy = sec.GetKeyData("proxy").Value;
+
+                liveUrl = sec["url"];
+
                 if (sec.ContainsKey("port"))
                     if (!int.TryParse(sec.GetKeyData("port").Value, out port))
-                        Console.WriteLine("配置文件解析错误：端口号无法解析，将使用默认端口号" + port);
+                        WriteLineWithColor("配置文件解析错误：端口号无法解析，将使用默认端口号" + port, ConsoleColor.Yellow);
+
+                if (ini.Sections.ContainsSection("youtube"))
+                {
+                    var sec2 = ini.Sections["youtube"];
+                    channelUrl = sec2["channel"];
+                }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
+                return;
+            }
+
+            if (!File.Exists(streamlink))
+            {
+                WriteLineWithColor("初始化失败：Streamlink未在指定路径中找到！", ConsoleColor.Red);
+                return;
+            }
+
+            try
+            {
+                if (channelUrl != null)
+                {
+                    Console.WriteLine($"已指定YouTube频道地址{channelUrl}，尝试获取直播间地址...");
+                    if (channelUrl.EndsWith("/") || !channelUrl.Contains("/"))
+                    {
+                        WriteLineWithColor($"无效地址{channelUrl}，正确的格式：https://www.youtube.com/channel/UCIaC5td9nGG6JeKllWLwFLA", ConsoleColor.Red);
+                        return;
+                    }
+                    var id = channelUrl.Substring(channelUrl.LastIndexOf('/') + 1);
+                    liveUrl = FetchYouTubeLiveUrl(id, proxy);
+                    if (!string.IsNullOrWhiteSpace(liveUrl))
+                        WriteLineWithColor($"获取到直播间地址：{liveUrl}", ConsoleColor.Green);
+                    else
+                    {
+                        WriteLineWithColor("获取失败！", ConsoleColor.Red);
+                        return;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                WriteLineWithColor("初始化失败：获取YouTube直播间地址失败！" + e, ConsoleColor.Red);
                 return;
             }
             
@@ -45,7 +95,7 @@ namespace StreamlinkCLI
                 Listener.Bind(new IPEndPoint(IPAddress.Any, port));
                 Listener.Listen(10);
 
-                Console.WriteLine($"[{DateTime.Now}] 服务器启动于端口{port}.");
+                WriteLineWithColor($"[{DateTime.Now}] 服务器启动于端口{port}.", ConsoleColor.Green);
                 // Start listening for connections.  
                 while (true)
                 {
@@ -54,7 +104,7 @@ namespace StreamlinkCLI
                     new Thread(() =>
                     {
                         var sid = new Random().Next(0, 10000);
-                        Console.WriteLine($"[{DateTime.Now}] [{sid}] 检测到客户端连接！通过Streamlink连接频道{channelUrl}，使用代理{proxy}...");
+                        WriteLineWithColor($"[{DateTime.Now}] [{sid}] 检测到客户端连接！通过Streamlink连接频道{liveUrl}，使用代理{proxy}...", ConsoleColor.Blue);
 
                         var recv = new byte[1024];
                         handler.Receive(recv);
@@ -63,11 +113,14 @@ namespace StreamlinkCLI
                         {
                             handler.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\nConnection: Closed\r\n\r\n"));
 
-                            var info = new ProcessStartInfo(streamlink,
-                                $"-O {channelUrl} best " +
-                                "--hls-timeout 1200 --stream-timeout 1200 --retry-streams 5 --retry-max 100000 --retry-open 500" +
-                                " --hls-segment-attempts 10 --hls-segment-timeout 60 --hls-timeout 600  --hls-segment-threads 3 --hls-live-edge 7 --hls-playlist-reload-attempts 10 " +
-                                $"--http-proxy={proxy} --https-proxy={proxy}")
+                            var arg = $"-O {liveUrl} best " +
+                                      "--hls-timeout 1200 --stream-timeout 1200 --retry-streams 5 --retry-max 100000 --retry-open 500" +
+                                      " --hls-segment-attempts 10 --hls-segment-timeout 60 --hls-timeout 600  --hls-segment-threads 3 --hls-live-edge 7 --hls-playlist-reload-attempts 10";
+                            if (!string.IsNullOrWhiteSpace(proxy))
+                            {
+                                arg += $" --http-proxy={proxy} --https-proxy={proxy}";
+                            }
+                            var info = new ProcessStartInfo(streamlink, arg)
                             {
                                 RedirectStandardError = true,
                                 RedirectStandardOutput = true,
@@ -104,8 +157,37 @@ namespace StreamlinkCLI
             }
             catch (Exception ex)
             {
+                Console.BackgroundColor = ConsoleColor.Red;
                 Console.WriteLine($"[{DateTime.Now}] 服务器监听线程出现异常！程序退出.\r\n{ex}");
+                Console.BackgroundColor = ConsoleColor.Black;
             }
+        }
+
+        private static readonly Regex YouTubeUrlRegex = new Regex("<link rel=\"canonical\" href=\"(.+)\">");
+        public static string FetchYouTubeLiveUrl(string id, string proxy)
+        {
+            var url = "https://www.youtube.com/embed/live_stream?channel=" + id;
+            var req = (HttpWebRequest) WebRequest.Create(url);
+            if (proxy != null)
+                req.Proxy = new WebProxy(new Uri(proxy));
+            req.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+            using (var resp = req.GetResponse())
+                using (var stream = resp.GetResponseStream())
+                using (var reader = new StreamReader(stream ?? throw new InvalidOperationException("无法获取响应流")))
+                {
+                    var html = reader.ReadToEnd();
+                    var matches = YouTubeUrlRegex.Matches(html);
+                    if (matches.Count == 0) throw new Exception("未找到有效的直播间地址，HTML数据：" + html + "\r\n\r\n");
+                    var liveUrl = matches[0].Groups[1].Value;
+                    return liveUrl;
+                }
+        }
+
+        public static void WriteLineWithColor(string str, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(str);
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
